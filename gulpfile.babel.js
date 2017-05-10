515ccf6b3b842ef8ea11a6f70c0839c9d52176fc
Arnelle Balane <arnellebalane@gmail.com>
2017-05-10 10:40:39 +0800

    configure gulp to build the hermes source files

---
+++ gulpfile.babel.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+
+gulp.task('default', () => {
+    gulp.src(['hermes.js', 'hermes-worker.js'])
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(rename((path) => path.extname = '.min.js'))
+        .pipe(gulp.dest('dist'));
+});
