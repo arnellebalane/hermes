519811677aad7d78426a7325c228b5ff07615e10
Valeriy Filin <valeriy.filin@iotium.io>
2020-05-21 17:41:24 -0700

    gulp upgrade

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,11 @@ import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 
-gulp.task('default', () => {
+gulp.task('default', done => {
     gulp.src(['hermes.js', 'hermes-worker.js'])
         .pipe(babel())
         .pipe(uglify())
         .pipe(rename(path => (path.extname = '.min.js')))
         .pipe(gulp.dest('dist'));
+    done();
 });
